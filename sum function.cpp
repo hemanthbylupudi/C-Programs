38749c404fcd385b6491471a2f214bd55bfc46b8
hemanthbylupudi <122376842+hemanthbylupudi@users.noreply.github.com>
2024-07-08 14:14:59 +0530

    Add files via upload

---
+++ sum function.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void sum()
+
+{ 
+ int a,b,sum=0;
+	printf("enter the values of a and b");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum of the numbers =%d",sum);
+	
+}
+void main()
+{
+return 0;
+}
+
